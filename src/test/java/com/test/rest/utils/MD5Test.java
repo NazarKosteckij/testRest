55f14b61e4852020e01ba8b01fbc4216d7f923b4
silver_925@ukr.net <silver_925@ukr.net>
2016-01-10 10:59:59 +0200

    fixed MD5.java add checking of password length add test cases

--- src/test/java/com/test/rest/utils/MD5Test.java
+++ src/test/java/com/test/rest/utils/MD5Test.java
@@ -1,20 +1,36 @@
 package com.test.rest.utils;
 
-import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MD5Test {
+	private final String ENCODED_PASSWORD = "b59c67bf196a4758191e42f76670ceba";
+	private final String PASSWORD = "1111";
+
+	private  MD5 md5;
 
 	@Before
 	public void setUp() throws Exception {
+		md5 = new MD5();
 	}
 
 	@Test
 	public void testGetMD5() {
-		String md5 = MD5.getMD5("1111");
-		assertTrue("b59c67bf196a4758191e42f76670ceba".equals(md5));
+		String encodedPassword = MD5.getMD5(PASSWORD);
+		assertEquals(ENCODED_PASSWORD, encodedPassword);
 	}
 
+	@Test
+	public void testEncode() throws Exception {
+		assertEquals(ENCODED_PASSWORD, md5.encode(PASSWORD));
+	}
+
+	@Test
+	public void testMatches() throws Exception {
+		assertEquals(true, md5.matches(ENCODED_PASSWORD, ENCODED_PASSWORD));
+		assertEquals(true, md5.matches(PASSWORD, ENCODED_PASSWORD));
+		assertEquals(false, md5.matches("foo", ENCODED_PASSWORD) );
+	}
 }
